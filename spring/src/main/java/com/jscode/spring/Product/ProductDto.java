a0795e0952f57691f395db95d309cbb0aa688c1e
KimSangHyeok <tkdgur0906@naver.com>
2023-03-18 23:48:21 +0900

    feat: Product Api 생성

---
+++ spring/src/main/java/com/jscode/spring/Product/ProductDto.java
@@ -0,0 +1,12 @@
+package com.jscode.spring.Product;
+
+import lombok.Getter;
+
+@Getter
+public class ProductDto {
+
+    private String name;
+
+    private int price;
+
+}
---
+++ spring/src/main/java/com/jscode/spring/Product/ProductRepository.java
@@ -0,0 +1,34 @@
+package com.jscode.spring.Product;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class ProductRepository {
+
+    private final static List<Product> store = new ArrayList<>();
+
+
+    public Long save(Product product) {
+        store.add(product);
+        return product.getId();
+    }
+
+
+    public Product findOne(String name) {
+        for (Product product : store) {
+            if (product.getName().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+
+    public List<Product> findAll() {
+        return new ArrayList<>(store);
+    }
+}
