c7cdf0fbbfab8fbb3ffc9a1ba17bf54cb8966aa9
KimSangHyeok <tkdgur0906@naver.com>
2023-03-23 21:47:38 +0900

    feat: ProductEntity name으로 찾는 메서드 추가

--- src/main/java/com/jscode/spring/repository/ProductJpaRepository.java
+++ src/main/java/com/jscode/spring/repository/ProductJpaRepository.java
@@ -4,5 +4,11 @@ package com.jscode.spring.repository;
 import com.jscode.spring.domain.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
+
+    public ProductEntity findByName(String name);
+
+    public List<ProductEntity> findAllByName(String name);
 }
